76fc766f40483afa679007977fe504c84eaff1a0
crashmax-dev <crashmax.dev@gmail.com>
2022-09-06 13:25:08 +0000

    Deploying to gh-pages from @ crashmax-dev/beautifier-userscript@cc23be0bf306f775d14cade6fad2a62cd6b24fdc 🚀

--- beautifier-userscript.meta.js
+++ beautifier-userscript.meta.js
@@ -1,7 +1,10 @@
 // ==UserScript==
-// @name     beautifier-userscript
-// @version  1.0.0
-// @match    https://beautifier.io
-// @grant    unsafeWindow
-// @grant    GM_addStyle
+// @name        beautifier-userscript
+// @version     1.0.0
+// @homepage    https://crashmax-dev.github.io/beautifier-userscript/
+// @match       https://beautifier.io
+// @grant       unsafeWindow
+// @grant       GM_addStyle
+// @updateURL   https://crashmax-dev.github.io/beautifier-userscript/beautifier-userscript.meta.js
+// @downloadURL https://crashmax-dev.github.io/beautifier-userscript/beautifier-userscript.user.js
 // ==/UserScript==
--- beautifier-userscript.user.js
+++ beautifier-userscript.user.js
@@ -1,9 +1,12 @@
 // ==UserScript==
-// @name     beautifier-userscript
-// @version  1.0.0
-// @match    https://beautifier.io
-// @grant    unsafeWindow
-// @grant    GM_addStyle
+// @name        beautifier-userscript
+// @version     1.0.0
+// @homepage    https://crashmax-dev.github.io/beautifier-userscript/
+// @match       https://beautifier.io
+// @grant       unsafeWindow
+// @grant       GM_addStyle
+// @updateURL   https://crashmax-dev.github.io/beautifier-userscript/beautifier-userscript.meta.js
+// @downloadURL https://crashmax-dev.github.io/beautifier-userscript/beautifier-userscript.user.js
 // ==/UserScript==
 
 (function(){"use strict";const i="";unsafeWindow.switchTheme({target:{checked:!0}}),GM_addStyle(".container{max-width:initial;display:initial}.options{display:none}.CodeMirror{font-size:100%;border:initial}.editor{padding-left:1rem;padding-right:1rem}")})();
